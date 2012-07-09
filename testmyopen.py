b39c4518fecc1b972d260a5c3ab0bced38ab521f
Raphaël Jacquot <sxpert@sxpert.org>
2012-07-09 12:09:30 +0200

    handle sockets in an object oriented fashion start adding more parsing (who=13)

--- testmyopen.py
+++ testmyopen.py
@@ -4,6 +4,10 @@ import socket, string
 
 DEBUG  = True
 
+class InvalidConnectionType (Exception):
+	def __init__ (self):
+		pass
+
 class InvalidPacket (Exception):
 	def __init__ (self, packet):
 		self._packet = packet
@@ -39,59 +43,90 @@ class ownNakPacket (ownPacket) :
 	def __str__ (self):
 		return 'NACK packet'
 
-def parse_message(msg) :
-	m = msg
+#------------------------------------------------------------------------------
+
+COMMAND = 1
+MONITOR = 2
+
+class ownSocket (object) :
+	def __init__ (self,address, port, socktype) :
+		self.address = address
+		self.port = port
+		self.socktype = socktype
+		self.buf = ''
+		self.sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((self.address,self.port))
+
+		self.handleMessage()
+		if self.socktype==COMMAND:
+			self.sock.send('*99*0##')
+		elif self.socktype==MONITOR:
+			self.sock.send('*99*1##')
+		else:
+			raise InvalidConnectionType 	
+ 
+	def parseMessage (self, msg) :
+		m = msg
 	
-	if DEBUG :
-		print '[DEBUG] '+m	
-
-	if m[0]!='*' : 
-		raise InvalidPacket(msg)
-	m = m[1:]
-	e = m[-2:]
-	if e != '##' :
-		raise InvalidPacket(msg)
-	m = m[0:-2]
-	normal=True
-	if m[0] == '#':
-		normal = False
+		if m[0]!='*' : 
+			raise InvalidPacket(msg)
 		m = m[1:]
-	# find WHO
-	p = m.find('*')
-	if p == -1:
-		raise UnknownPacket(msg)
-	who = m[0:p]
-	m = m[(p+1):]
-	if len(who) == 0:
-		who = None
-	else :
-		# check if all chars of who are digits
-		for c in who :
-			if c not in string.digits:
-				raise InvalidPacket(msg)
-		who = int(who)
-	if who is None: 
-		if m == '0' :
-			return ownNackPacket()
-		if m == '1' : 
-			return ownAckPacket()	
-	else:	
-		raise UnknownWho(who, msg)
+		normal=True
+		if m[0] == '#':
+			normal = False
+			m = m[1:]
+		# find WHO
+		p = m.find('*')
+		if p == -1:
+			raise UnknownPacket(msg)
+		who = m[0:p]
+		m = m[(p+1):]
+		if len(who) == 0:
+			who = None
+		else :
+			# check if all chars of who are digits
+			for c in who :
+				if c not in string.digits:
+					raise InvalidPacket(msg)
+			who = int(who)
+		if who is None: 
+			if m == '0' :
+				return ownNackPacket()
+			if m == '1' : 
+				return ownAckPacket()	
+		elif who == 13:
+			# gateway control
+			return self.parseGateway(m, msg)
+		else:	
+			raise UnknownWho(who, msg)
+	
+	def parseGateway(self, m, msg) :
+		print 'Gateway message '+m
+		return None
 
-def handle_message(s) :
-	msg = s.recv(1024)
-	try :
-		m = parse_message(msg)
-	except UnknownPacket, e:
-		m = 'Unknown Packet \'' + str(e) + '\''
-	except UnknownWho, e:
-		m = 'Unknown WHO value [' + str(e) + ']'	
-	print '[MON] '+str(m)
+	def handleMessage (self) :
+		p = self.buf.find('##')
+		if p==-1 :
+			msg = self.sock.recv(1024)
+			self.buf+=msg
+			p = self.buf.find('##')
+			if p==-1 : 
+				return
+		msg = self.buf[0:p]
+		self.buf = self.buf[(p+2):]
+		m = None
+		try :
+			m = self.parseMessage(msg)
+		except UnknownPacket, e:
+			msg = 'Unknown Packet \'' + str(e) + '\''
+		except UnknownWho, e:
+			msg = 'Unknown WHO value [' + str(e) + ']'	
+		else:
+			msg = str(m) 
+		print '[MON] '+msg
+		return m
 
 if __name__ == '__main__':
-	s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('f454',20000))
-	handle_message(s)
-	s.send('*99*1##')
+	s = ownSocket('f454',20000,MONITOR)
 	while True:
-		handle_message(s)
+		s.handleMessage()
