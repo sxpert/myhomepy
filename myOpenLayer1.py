5c09d51b1b7f9634f538c3cd77334c89f449681d
Raphaël Jacquot <sxpert@sxpert.org>
2016-01-28 13:06:13 +0100

    work on socket timeouting, and parsing of messages in the test app

--- myOpenLayer1.py
+++ myOpenLayer1.py
@@ -9,7 +9,7 @@
 from __future__ import print_function
 
 # system includes
-import sys, select, socket, string, types, re, time
+import sys, select, socket, string, types, re, time, errno
 import datetime
 # application includes
 import myOpenPass
@@ -47,193 +47,213 @@ system_logger = Logger (logfile)
 #
 
 class MainLoop (object) :
-	def __init__ (self, logger, timeout = 200) :
-		self.sockets = {}
-		self.timers = []
-		self.logger = logger
-		self.timeout = timeout 
-		self.poller = select.poll()
+    def __init__ (self, logger, timeout = 200) :
+        self.sockets = {}
+        self.timers = []
+        self.logger = logger
+        self.timeout = timeout 
+        self.poller = select.poll()
 
-	def add_socket (self, socket):
-		# get socket to connect
-		socket.connect()
-		if socket.sock is not None :
-			fd = socket.sock.fileno()
-			self.sockets[fd] = socket
-			self.poller.register(socket.sock, select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR)
-		else :
-			self.logger.log ('unable to add socket to poller, sock==None')
+    def add_socket (self, socket):
+        # get socket to connect
+        socket.connect()
+        if socket.sock is not None :
+            fd = socket.sock.fileno()
+            self.sockets[fd] = socket
+            self.poller.register(socket.sock, select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR)
+        else :
+            self.logger.log ('unable to add socket to poller, sock==None')
 
-	def remove_socket (self, socket) :
-		if socket is not None :
-			if socket.sock is not None :
-				self.poller.unregister (socket.sock)
-				fd = socket.sock.fileno()
-				del self.sockets[fd]
+    def remove_socket (self, socket) :
+        if socket is not None :
+            if socket.sock is not None :
+                self.poller.unregister (socket.sock)
+                fd = socket.sock.fileno()
+                del self.sockets[fd]
 
-	def add_timer (self, ts, callback):
-		self.timers.append((ts,callback))
-		self.timers = sorted(self.timers, key=lambda timer: timer[0])
+    def add_timer (self, ts, callback):
+        self.timers.append((ts,callback))
+        self.timers = sorted(self.timers, key=lambda timer: timer[0])
 
-	def run (self) :
-		while True :
-			if self.sockets is not None :
-				t = self.timeout
-			else :
-				t = 60000;
-			try :
-				events = self.poller.poll(t)
-				if len(events) > 0 : 	
-					for e in events :
-						fd, flags = e
-						s = self.sockets[fd]
-						if flags & (select.POLLIN | select.POLLPRI) :
-							s.read()
-						elif flags & select.POLLHUP :
-							self.logger.log ('socket '+str(s)+' is hanged-up')
-							self.poller.unregister (socket.sock)
-							# remove socket from dict
-							del self.sockets[fd]
-							s.reconnect(self)
-						elif flags & select.POLLERR :
-							self.logger.log ('socket '+str(s)+' is in error state')
-							self.poller.unregister (socket.sock)
-							del self.sockets[fd]
-							s.reconnect(self)
-				# handle timers
-				while len(self.timers)>0 :
-					t = self.timers[0]
-					ts = t[0]
-					now = time.time()
-					if now<ts :
-						break
-					self.timers = self.timers[1:]
-					t[1]()
-			except KeyboardInterrupt as e:
-				self.logger.log ("program exit")
-				sys.exit(0)	
-	
+    def run (self) :
+        while True :
+            if self.sockets is not None :
+                t = self.timeout
+            else :
+                t = 60000;
+            try :
+                events = self.poller.poll(t)
+                if len(events) > 0 :
+                    for e in events :
+                        fd, flags = e
+                        s = self.sockets[fd]
+                        if flags & (select.POLLIN | select.POLLPRI) :
+                            try: 
+                                s.read()
+                            except socket.error as e :
+                                s.log (unicode(e))
+                                if e.errno == errno.ETIMEDOUT :
+                                    s.close()
+                        elif flags & select.POLLHUP :
+                            self.logger.log ('socket '+str(s)+' is hanged-up')
+                            self.poller.unregister (socket.sock)
+                            # remove socket from dict
+                            del self.sockets[fd]
+                            s.reconnect(self)
+                        elif flags & select.POLLERR :
+                            self.logger.log ('socket '+str(s)+' is in error state')
+                            self.poller.unregister (socket.sock)
+                            del self.sockets[fd]
+                            s.reconnect(self)
+                # handle timers
+                while len(self.timers)>0 :
+                    t = self.timers[0]
+                    ts = t[0]
+                    now = time.time()
+                    if now<ts :
+                        break
+                    self.timers = self.timers[1:]
+                    t[1]()
+                # check if one of the socket is disconnected
+                for fd in self.sockets.keys() :
+                    s = self.sockets[fd]
+                    if s.sock is None :
+                        self.logger.log ('socket '+unicode(fd)+' disconnected')
+                        s.connect ()
+                        newfd = s.sock.fileno()
+                        if fd != newfd :
+                            self.logger.log ('socket '+unicode(fd)+' connected with fd '+unicode(newfd))
+            except KeyboardInterrupt as e:
+                self.logger.log ("program exit")
+                sys.exit(0)
+
 #--------------------------------------------------------------------------------------------------
 #
 # Layer 1 : tcp socket handling
 #
 
 class OwnSocket (object) :
-	COMMAND = 1
-	MONITOR = 2
-	MODES = {
-		COMMAND: 'CMD',
-		MONITOR: 'MON'
-	}
-	NONE    = 0
-	LOGGING = 1
-	AUTH    = 2
-	LOGGED  = 3
-	FAILED  = 4
+    COMMAND = 1
+    MONITOR = 2
+    MODES = {
+        COMMAND: 'CMD',
+        MONITOR: 'MON'
+    }
+    NONE    = 0
+    LOGGING = 1
+    AUTH    = 2
+    LOGGED  = 3
+    FAILED  = 4
 
-	NACK    = '*#*0##'
-	ACK     = '*#*1##'
+    NACK    = '*#*0##'
+    ACK     = '*#*1##'
 
-	def __init__ (self, address, port, passwd, mode) :
-		self.address = address
-		self.port = port
-		self.passwd = passwd
-		self.mode = mode
-		self.buf = ''
-		self.sock = None
-		self.state = self.NONE
-		self.ready_callback = None
-		self.data_callback = None
-		
-	def log (self, msg) :
-		m = '['+self.address+':'+str(self.port)+' '+self.MODES[self.mode]+'] '+msg
-		system_logger.log(m)
+    def __init__ (self, address, port, passwd, mode) :
+        self.address = address
+        self.port = port
+        self.passwd = passwd
+        self.mode = mode
+        self.buf = ''
+        self.sock = None
+        self.state = self.NONE
+        self.ready_callback = None
+        self.data_callback = None
 
-	def connect (self) :
-		self.sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)		
-		try :
-			self.sock.connect ((self.address, self.port))
-		except socket.error as e :
-			self.sock = None
-			self.log ("connection error, sleeping some")
-			# this should setup some timer
-			return		
-		self.sock.setblocking (0)
+    def log (self, msg) :
+        m = '['+self.address+':'+str(self.port)+' '+self.MODES[self.mode]+'] '+msg
+        system_logger.log(m)
 
-	def close (self) :
-		if self.sock is not None : 
-			#self.log ('OwnSocket : Closing socket')
-			self.sock.close()
+    def connect (self) :
+        self.state = self.NONE
+        self.sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        # set keepalive options
+        self.sock.setsockopt (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        self.sock.setsockopt (socket.SOL_TCP, socket.TCP_KEEPIDLE, 1)
+        self.sock.setsockopt (socket.SOL_TCP, socket.TCP_KEEPINTVL, 1)
+        self.sock.setsockopt (socket.SOL_TCP, socket.TCP_KEEPCNT, 2)
+        try :
+            self.sock.connect ((self.address, self.port))
+        except socket.error as e :
+            self.sock = None
+            self.log ("connection error, sleeping some")
+            # this should setup some timer
+            return
+        self.sock.setblocking (0)
 
-	def reconnect (self, mainloop) :
-		# attempt to reconnect the socket
-		self.log ("attempting to reconnect")
-		pass
+    def close (self) :
+        if self.sock is not None : 
+            self.log ('OwnSocket : Closing socket')
+            self.sock.close()
+            self.sock = None
 
-	def read (self) :
-		data = self.sock.recv(1024)
-		self.buf += data 
-		while True :
-			p = self.buf.find ('##')
-			if p==-1 :
-				return
-			msg = self.buf[0:p+2]
-			self.buf = self.buf[(p+2):]
-			if self.state==self.NONE :
-				if msg==self.ACK :
-					self.set_socket_mode()
-					return
-				else :
-					self.log('Invalid message received : '+msg)
-					return
-			elif self.state==self.LOGGING :
-				p = re.compile('\*#(\d+)##')
-				m = p.match(msg)
-				if m is not None :
-					nonce = m.group(1)
-					self.send_response (nonce)
-				else :
-					self.log ('Unable to find Nonce in message '+msg+'##')
-				return
-			elif self.state==self.AUTH :
-				if msg==self.ACK :
-					self.log ('successfully connected')
-					self.state=self.LOGGED
-					if self.ready_callback is not None :
-						self.ready_callback()
-				else:
-					self.log ('unable to log in')
-					self.state=self.FAILED
-				return
-			# TODO: do things with messages
-			if self.data_callback is not None :
-			    self.data_callback(msg)
-			else :
-			    self.log(msg)
+    def reconnect (self, mainloop) :
+        # attempt to reconnect the socket
+        self.log ("attempting to reconnect")
 
-	def set_socket_mode (self) :
-		self.log('initializing connection')
-		if self.mode == self.COMMAND :
-			self.sock.send('*99*0##')
-			self.state = self.LOGGING
-		elif self.mode == self.MONITOR :
-			self.sock.send('*99*1##')
-			self.state = self.LOGGING
-		else :
-			self.log ("unknown connection type : "+str(self.mode))
-		
-	def send_response (self, nonce) :
-		self.log ('got nonce '+nonce)
-		p = '*#'+str(myOpenPass.ownCalcPass (self.passwd, nonce))+'##'
-		self.log ('logging in with password packet '+p)
-		self.state = self.AUTH
-		self.sock.send (p)
+    def read (self) :
+        data = self.sock.recv(1024)
+        self.buf += data 
+        while True :
+            p = self.buf.find ('##')
+            if p==-1 :
+                 return
+            msg = self.buf[0:p+2]
+            self.buf = self.buf[(p+2):]
+            if self.state==self.NONE :
+                if msg==self.ACK :
+                    self.set_socket_mode()
+                    return
+                else :
+                    self.log('Invalid message received : '+msg)
+                    return
+            elif self.state==self.LOGGING :
+                p = re.compile('\*#(\d+)##')
+                m = p.match(msg)
+                if m is not None :
+                    nonce = m.group(1)
+                    self.send_response (nonce)
+                else :
+                    self.log ('Unable to find Nonce in message '+msg+'##')
+                return
+            elif self.state==self.AUTH :
+                if msg==self.ACK :
+                    self.log ('successfully connected')
+                    self.state=self.LOGGED
+                    if self.ready_callback is not None :
+                        self.ready_callback()
+                else:
+                    self.log ('unable to log in')
+                    self.state=self.FAILED
+                return
+            # TODO: do things with messages
+            if self.data_callback is not None :
+                self.data_callback(msg)
+            else :
+                self.log(msg)
 
-	def set_ready_callback (self, callback) :
-		self.ready_callback = callback
+    def set_socket_mode (self) :
+        self.log('initializing connection')
+        if self.mode == self.COMMAND :
+            self.sock.send('*99*0##')
+            self.state = self.LOGGING
+        elif self.mode == self.MONITOR :
+            self.sock.send('*99*1##')
+            self.state = self.LOGGING
+        else :
+            self.log ("unknown connection type : "+str(self.mode))
 
-	def set_data_callback (self, callback) :
-		self.data_callback = callback
+    def send_response (self, nonce) :
+        self.log ('got nonce '+nonce)
+        p = '*#'+str(myOpenPass.ownCalcPass (self.passwd, nonce))+'##'
+        self.log ('logging in with password packet '+p)
+        self.state = self.AUTH
+        self.sock.send (p)
+
+    def set_ready_callback (self, callback) :
+        self.ready_callback = callback
+
+    def set_data_callback (self, callback) :
+        self.data_callback = callback
 
 #--------------------------------------------------------------------------------------------------
 #
