11f73da91775082ac949a4f1e48e8c7598662fd8
Raphaël Jacquot <sxpert@sxpert.org>
2017-03-16 15:41:46 +0100

    start of actions module

---
+++ actions.py
@@ -0,0 +1,16 @@
+
+
+
+#
+# takes data from actions.json
+# executes the series of requests
+#
+class ActionEngine (object):
+    def __init__ (self):
+        # load the json
+
+    
+
+class ScanNetwork (object):
+    def __init__ (self):
+
