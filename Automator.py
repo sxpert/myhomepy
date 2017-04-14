ebeddcae5c471191c4094b1aecef86d964696afd
Raphaël Jacquot <sxpert@sxpert.org>
2017-04-14 15:45:35 +0200

    * Automator.py: Cleanup * index.html: add stylesheet * main.css: add * myhomepy-ui.js: add the resize-bar object, handle adding the contents widow

--- Automator.py
+++ Automator.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python2.7 -3
 #-*- coding: utf-8 -*-
 
+"""
+Main application module
+"""
+
 #
 # (c) Raphael Jacquot 2014
 # Licenced under the terms of the GNU GPL v3.0 or later
@@ -8,35 +12,37 @@
 
 #import config
 import myOpenLayer1
-import myOpenLayer2
 import config
 import webserver
 import website
-    
-class TestApplication (object) :
 
-    def __init__ (self) :
+class TestApplication(object):
+    """
+    This is the test application object
+    """
+
+    def __init__(self):
         # create the system loop
-        self.sl = myOpenLayer1.MainLoop(myOpenLayer1.system_logger)
-        config.config.set_main_loop(self.sl)
+        self.system_loop = myOpenLayer1.MainLoop(myOpenLayer1.system_logger)
+        config.config.set_main_loop(self.system_loop)
 
         # initializes the web server
         addr = ('', 8000)
         self.web = webserver.OpenWeb(addr)
-    
-        self.web.register_routes (
+
+        self.web.register_routes(
             [
-                [ "^/API/$", website.ow_index.OW_index ],
-                [ "^/API/add_system(.*)$", website.ow_add_system.OW_add_system ],
+                ["^/API/$", website.ow_index.OW_index],
+                ["^/API/add_system(.*)$", website.ow_add_system.OW_add_system],
             ]
         )
-        self.web.default_route (website.ow_static.OW_static)
+        self.web.default_route(website.ow_static.OW_static)
 
-        self.sl.add_server (self.web)
+        self.system_loop.add_server(self.web)
 
-        self.sl.run ()
+        self.system_loop.run()
 
 # main program
-if __name__ == '__main__' :
+if __name__ == '__main__':
     # create the application object, run the main loop
-    TestApplication ()
+    TestApplication()
--- static/js/myhomepy-ui.js
+++ static/js/myhomepy-ui.js
@@ -22,13 +22,74 @@ class WindowTab {
 
     click(event) {
         console.log(this.tab_label+' clicked');
+        this.main.set_window(this.config_window)
     }
 }
 
 class ConfigTab extends WindowTab {
     initialize() {
         this.tab_label = 'Configuration';
+        this.config_window = new ConfigWindow();
         super.initialize();
+    }
+}
+
+class ResizeBar {
+    constructor(){
+        console.log('resizer-bar constructor');
+    }
+    generate_html(){
+        var bar = document.createElement('div');
+        bar.setAttribute('class','resize-bar');
+        var resize_bar = this;
+        this.mouse_down_handler = function(e){
+                var b = resize_bar;
+                console.log(b);
+                b.mouse_down(e);
+            };
+        bar.addEventListener('mousedown', this.mouse_down_handler);
+        this.bar = bar;
+        return bar;
+    }
+    mouse_down(e){
+        e.target.setCapture(true);
+        var resize_bar = this;
+        this.mouse_move_handler = function(e){
+                var b = resize_bar;
+                b.mouse_move(e);
+            };
+        e.target.addEventListener('mousemove', this.mouse_move_handler);
+        this.mouse_up_handler = function(e){
+                var b = resize_bar;
+                b.mouse_up(e);
+            };
+        e.target.addEventListener('mouseup', this.mouse_up_handler);
+    }
+    mouse_move(e){
+        console.log(e.clientX+' '+e.clientY);
+    }
+    mouse_up(e){
+        e.target.removeEventListener('mousemove', this.mouse_move_handler);
+        e.target.removeEventListener('mouseup', this.mouse_up_handler);
+        document.releaseCapture();
+    }
+}
+
+class ConfigWindow {
+    constructor() {
+        console.log('Configuration window created');
+    }
+    generate_html() {
+        this.tree = $('<div class="options-tree">');
+        this.resize_bar = new ResizeBar();
+        var jqrb = $(this.resize_bar.generate_html());
+        this.detail = $('<div class="detail-container">');
+        var subwindows = [
+            this.tree,
+            jqrb,
+            this.detail
+        ];
+        return subwindows;
     }
 }
 
@@ -75,7 +136,20 @@ class MainWindow {
         this.tabs.push (tab);
     }
 
-
+    set_window(window) {
+        this.active_window = window;
+        console.log(this.active_window);
+        if (this.window_container === undefined) {
+            this.window_container = $('<div id="main">');
+            this.window_container.appendTo(this.body);
+        }
+        var contents = this.window_container.children();
+        console.log('contents', contents);
+        if (contents.length > 0) {
+            contents.remove();
+        }
+        this.window_container.append(this.active_window.generate_html());
+    }
 }
 
 function myHomePy_UI() {
