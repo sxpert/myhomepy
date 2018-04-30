2db3cf311f8d5683bd0d9015e383b2a87914e73b
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2018-04-30 12:06:37 +0200

    more PEP8 fixes

--- website/ow_static.py
+++ website/ow_static.py
@@ -3,25 +3,27 @@
 
 # myOpenApplication webserver
 
-import os, sys
+import os
 import re
+import sys
 import urllib
-from myopen import layer1
+
 import config
+from myopen import layer1
 
 STATIC_FILES = "static"
 
-#--------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------
 #
 # TODO: use a templating system
 #
 
-#----
+# ----
 # handle static content
-# 
+#
 
 class OW_static (object):
     def do_GET(self, request):
         # find this file in the static directory
         request.file_response(STATIC_FILES, request.path[1:])
-
