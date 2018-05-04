9add6b3f0ed976b0400083dda080b811c909658e
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2018-05-04 17:06:16 +0200

    fix the temperature reporting for now

--- website/ow_temperatures.py
+++ website/ow_temperatures.py
@@ -9,12 +9,13 @@ from myopen import layer1
 class OW_list_temperatures (object):
 
     def do_GET(self, request):
+        sensors = None
         if request.config.nb_systems == 1:
             # TODO: we may not have a database in this system
-            sensors = request.config.systems[0]\
-                        .monitor.database.list_temperature_sensors()
-        else:
-            sensors = None
+            system = request.config.systems[0]
+            if system.database is not None:
+                database = system.monitor.database
+                sensors = database.list_temperature_sensors()
         data = {
             'start': '2017-07-01T23:53',
             'end': '2018-01-25T23:59',
