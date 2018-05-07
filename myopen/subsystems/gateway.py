dc56b5be5c83686f4ec0d217af2f4c99b24b595b
Raphaël Jacquot <sxpert@mac2.sxpert.org>
2018-05-07 08:36:58 +0200

    parse the gateway date and time message, pretend they are being handled

--- myopen/subsystems/gateway.py
+++ myopen/subsystems/gateway.py
@@ -4,3 +4,18 @@ from .subsystem import OWNSubSystem
 
 class Gateway(OWNSubSystem):
     SYSTEM_WHO = 13
+
+    SYSTEM_REGEXPS = {
+        'Status': [
+            (r'^\*\*#0\*(?P<hour>[0-2][0-9])\*(?P<minute>[0-5][0-9])\*(?P<second>[0-5][0-9])\*(?P<timezone>\d{1,3})##$', '_time_info', ),
+            (r'^\*\*#1\*(?P<weekday>0[0-6])\*(?P<day>[0-3][0-9])\*(?P<month>[01][0-9])\*(?P<year>\d{4})##$', '_date_info', ),
+        ]
+    }
+
+    def _time_info(self, matches):
+        self.log('Gateway._date_info %s' % (str(matches)))
+        return True
+    
+    def _date_info(self, matches):
+        self.log('Gateway._date_info %s' % (str(matches)))
+        return True
