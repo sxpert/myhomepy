6c197e4900dd712524c4b3a78a1e323653180a31
Raphaël Jacquot <sxpert@mac2.sxpert.org>
2018-05-08 19:04:31 +0200

    cleanup

--- webserver.py
+++ webserver.py
@@ -346,22 +346,6 @@ class OpenWebHandler(http.server.BaseHTTPRequestHandler, object):
                 v += c
         return v
 
-
-# --------------------------------------------------------------------------------------------------
-#
-# TODO: ssl mode
-#
-# import BaseHTTPServer, SimpleHTTPServer
-# import ssl
-#
-# httpd = BaseHTTPServer.HTTPServer(('localhost', 4443),
-#                                   SimpleHTTPServer.SimpleHTTPRequestHandler)
-# httpd.socket = ssl.wrap_socket (httpd.socket,
-#                                 certfile='path/to/localhost.pem',
-#                                 server_side=True)
-# httpd.serve_forever()
-#
-
 #
 #
 # web server object
