2ad5fe03598b597104453798a2d6a769cd51331c
Raphaël Jacquot <sxpert@mac2.sxpert.org>
2018-05-14 08:25:27 +0200

    vela is a legrand line

--- myopen/devices/basedevice.py
+++ myopen/devices/basedevice.py
@@ -63,6 +63,8 @@ class BaseDevice(json.JSONEncoder):
                 'long': 'Legrand'
             },
             'lines': {
+                PROD_LINE_VELA: 'Vela',
+                PROD_LINE_ARTEOR: 'Arteor',
                 PROD_LINE_MOSAIC: 'Mosaic',
                 PROD_LINE_CELIANE: 'Céliane',
             }
@@ -118,6 +120,7 @@ class BaseDevice(json.JSONEncoder):
         sys_who = getattr(subsystem, 'SYSTEM_DIAG_WHO', None)
         if sys_who is None:
             print('PANIC : subsystem %s has no SYSTEM_DIAG_WHO' % (str(subsystem)))
+            return None
         if dev_sys is not None and dev_sys != sys_who:
             print('PANIC : subsystem %s, device %d' % (str(subsystem), dev_sys))
             return None
