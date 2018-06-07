a572d8cd5622de2392248f390cd41e1a783335ce
Raphaël Jacquot <sxpert@mac2.sxpert.org>
2018-06-07 19:34:03 +0200

    forgot to set the slot

--- myopen/devices/dev_bt_4693/bt_4693.py
+++ myopen/devices/dev_bt_4693/bt_4693.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from ..basedevice import BaseDevice
 from ...subsystems.temp_control import TempControl
+from ..basedevice import BaseDevice
+from .bt_4693_slot import Device4693_Slot
 
 
 class Device4693(BaseDevice):
     DEVICE_SYSTEM = TempControl
     MODEL_ID = 21
+    SLOT_CLASS = Device4693_Slot
