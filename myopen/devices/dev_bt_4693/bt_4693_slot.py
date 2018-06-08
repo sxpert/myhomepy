5bae18822380cc89d32321ec2a9a6694a3cdabdd
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2018-06-08 12:15:04 +0200

    work on kos and stuff

--- myopen/devices/dev_bt_4693/bt_4693_slot.py
+++ myopen/devices/dev_bt_4693/bt_4693_slot.py
@@ -4,20 +4,27 @@ from core.logger import LOG_ERROR, LOG_DEBUG
 from ..baseslot import F_KO, F_SYS_ADDRESS, BaseSlot
 from ..dev_utils import split_long_addr
 
-KO_UNCONFIGURED = 221  # houston, we have a problem !
-KO_MASTER_PROBE = 184
+KO_UNCONFIGURED = 508  # note: MyHomeSuite doesn't seem to use this !
+KO_TEMP_CONTROL_PROBE = 36  # note: for firmware 5.2.0 
+KO_MASTER_PROBE = 184  # note: for firmware 6.0.0
 KO_SLAVE_PROBE = 221
 
-KOS = {
-    'values': [KO_MASTER_PROBE, KO_SLAVE_PROBE],
-    'ids': ['MASTER_PROBE', 'SLAVE_PROBE'],
-    'names': ['Master probe', 'Slave probe']
+KOS_FW_5_2_0 = {
+    'values': [KO_UNCONFIGURED, KO_TEMP_CONTROL_PROBE, KO_SLAVE_PROBE],
+    'ids': ['UNCONFIGURED', 'TEMP_CONTROL_PROBE', 'SLAVE_PROBE'],
+    'names': ['Unconfigured', 'Temperature control probe', 'Slave probe']
+}
+
+KOS_FW_6_0_0 = {
+    'values': [KO_UNCONFIGURED, KO_MASTER_PROBE, KO_SLAVE_PROBE],
+    'ids': ['UNCONFIGURED', 'MASTER_PROBE', 'SLAVE_PROBE'],
+    'names': ['Unconfigured', 'Master probe', 'Slave probe']
 }
 
 FIELDS = {
     F_KO: {
         'param': F_KO,
-        'values': ['list', KOS],
+        'values': ['list', KOS_FW_6_0_0],
         'disp': {'label': 'Operating mode', 'order': 0}
     },
     F_SYS_ADDRESS: {
