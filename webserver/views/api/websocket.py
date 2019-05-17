2ecb5b96714da166e4eedfd2a6e563588d1f004e
Raphael Jacquot <raphael.jacquot@univ-grenoble-alpes.fr>
2019-05-17 11:06:22 +0200

    change log level

--- webserver/views/api/websocket.py
+++ webserver/views/api/websocket.py
@@ -6,7 +6,7 @@ from aiohttp import WSMsgType, web
 from aiohttp_session import get_session
 
 from ...decorators import login_required
-
+from core.logger import LOG_DEBUG
 
 class WebSocket(web.View):
     @login_required
@@ -15,7 +15,7 @@ class WebSocket(web.View):
         print('WEBSOCKET preparing')
         ws = web.WebSocketResponse()
         await ws.prepare(self.request)
-        config.log("WEBSOCKET: initialized with %s"%(str(ws._writer.transport.get_extra_info('peername'))))
+        config.log("WEBSOCKET: initialized with %s"%(str(ws._writer.transport.get_extra_info('peername'))), LOG_DEBUG)
         print('WEBSOCKET register')
         config.websocket_register(ws)
 
@@ -23,7 +23,7 @@ class WebSocket(web.View):
             print('WEBSOCKET', msg.type)
             if msg.type == WSMsgType.TEXT:
                 if msg.data == 'close':
-                    config.log("websocket closing %s"%(str(ws._writer.transport.get_extra_info('peername'))))
+                    config.log("websocket closing %s"%(str(ws._writer.transport.get_extra_info('peername'))), LOG_DEBUG)
                     await ws.close()
                 else:
                     print('WEBSOCKET', msg.data)
